1a982955d1e881ef5212126de0d072e9d0b07efe
Michael L Bangura <michlawbang123@gmail.com>
2022-09-03 04:34:36 -1200

    done

--- front-end/src/requestController.js
+++ front-end/src/requestController.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const BASE_URL = "http://localhost:5000/api/";
+const BASE_URL = "https://mern--shop.herokuapp.com/api/";
 
 
 export const publicRequest = axios.create({
